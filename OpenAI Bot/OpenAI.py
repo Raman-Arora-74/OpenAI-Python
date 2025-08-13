fd82d0d32cdb6597ad7ed9c30ff83575fb77387a
Raman-Arora-74 <ramandeepsingharora91@gmail.com>
2025-08-13 21:21:42 +0530

    Smart ChatBot add chat and enjoy

---
+++ OpenAI Bot/OpenAI.py
@@ -0,0 +1,30 @@
+from openai import OpenAI 
+client = OpenAI()
+messages = [
+    {"role":"system","content":"you are a kind assistant,friendly and explain things with real time examples"}
+]
+def chat():
+    while True:
+        msg = input(str("You :")).strip()
+
+# here u can write any specific word which help to exit from the loop 
+
+        if msg.capitalize() in ["Bye","Exit","Goodbye","See ya"]: 
+            print("Chatbot : Goodbye! See ya.")  
+            break  
+        dic = {"role":"user","content":msg}
+        messages.append(dic)
+        response = client.chat.completions.create(
+
+# choose any model    ⬇️       here 
+
+            model='gpt-4.1-mini',
+            messages=messages 
+        )        
+        reply = response.choices[0].message.content
+        print("Japu de sali:", reply)
+        messages.append({"role": "assistant", "content": reply})
+
+chat()
+
+
